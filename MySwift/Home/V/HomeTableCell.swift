d09e65e34d451bdcff00db48ff5b6ac21a0620ef
hechuan <hccgk@sina.com>
2019-04-02 11:00:51 +0800

    网络和布局

---
+++ MySwift/Home/V/HomeTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableCell.swift
+//  MySwift
+//
+//  Created by 何川 on 2019/4/2.
+//  Copyright © 2019 hechuan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios,"9.0"
+
+target 'MySwift' do
+pod 'SnapKit', '~> 0.30.0.beta1'
+pod 'Alamofire', '~> 3.4.1'
+end
